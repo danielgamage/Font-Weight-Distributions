8d59fe5a9ab03be6410352bccd124e9b375a44f1
Daniel Gamage <hello@danielgamage.com>
2017-02-25 17:58:14 -0500

    basic chart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  VictoryChart,
+  VictoryGroup,
+  VictoryAxis,
+  VictoryScatter,
+  VictoryLine,
+  VictoryVoronoiTooltip
+} from 'victory';
+
+const fonts = [
+  {
+    name: "Alloca Mono",
+    interpolations: [
+      { style: 'Thin',        weight:  10 },
+      { style: 'Extra Light', weight:  24 },
+      { style: 'Light',       weight:  42 },
+      { style: 'Book',        weight:  64 },
+      { style: 'Regular',     weight:  90 },
+      { style: 'Medium',      weight: 122 },
+      { style: 'Bold',        weight: 160 },
+      { style: 'Black',       weight: 200 }
+    ]
+  },
+  {
+    name: "Godia",
+    interpolations: [
+      { style: 'Regular',      weight:  80 },
+      { style: 'Bold',         weight: 200 }
+    ]
+  }
+]
+;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <VictoryChart
+          domainPadding={20}
+        >
+          <VictoryAxis
+            tickFormat={(el) => `${el}`}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(el) => `${el}`}
+          />
+        {fonts.map(font => (
+          <VictoryGroup
+            data={font.interpolations}
+            x="style"
+            y="weight" >
+            <VictoryLine
+              interpolation="catmullRom"
+              />
+            <VictoryScatter />
+          </VictoryGroup>
+        ))}
+        </VictoryChart>
       </div>
     );
   }
