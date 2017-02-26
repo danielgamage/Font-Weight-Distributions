f42dce9848107541b7b5b099f2b936d2b437d15c
Daniel Gamage <hello@danielgamage.com>
2017-02-26 06:02:45 -0500

    script working

--- getData.js
+++ getData.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const fs = require('fs')
 
 // YOU MUST SUPPLY YOUR OWN TOKEN FILE
 const token = require('./token.js').default
@@ -15,11 +16,13 @@ const glyphsURL = 'https://api.github.com/search/code?q=interpolationWeight+exte
 async function getGlyphsData() {
   try {
     const searchResults = await axios(glyphsURL, options )
-    const fontFamilies = searchResults.data.items.map(async (el) => {
+    const fontFamilies = Promise.all(searchResults.data.items.map(async (el) => {
       const file = await axios(el.url, options)
       const fontFamily = parseGlyphsFile(file.data.content)
       return fontFamily
-    })
+    }))
+    console.log(await fontFamilies)
+    writeToDisk(await fontFamilies)
   } catch (err) {
     console.log('failed', err)
   }
@@ -27,21 +30,32 @@ async function getGlyphsData() {
 
 getGlyphsData()
 
+const writeToDisk = (data) => {
+  fs.writeFile("./src/data/fonts.json", JSON.stringify(data), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  });
+}
+
 const parseGlyphsFile = (fileBuffer) => {
   const file = new Buffer(fileBuffer, 'base64').toString('ascii')
-  console.log(file)
-  const familyName = file.match(/familyName = "(.*)";/)[1]
-  const instanceMatches = file.match(/\{\n?(interpolationWeight[.\S\s]*?)\}[,\n]/)
-  // console.log(instanceMatches)
+  const familyName = file.match(/familyName = "(.*)";/)
+  const instanceMatches = file.match(/\{\n?((?:interpolationWeight|customParameters \= \([.\S\s]*?\);)[.\S\s]*?)(?:\}[,\n])/g)
   const instances = instanceMatches.map(instance => {
-    const name = instance.match(/name \= (.*)\;/)[1]
-    const weight = instance.match(/interpolationWeight \= (\d*)\;/)[1]
-    return { "style": name, "weight": weight }
+    const name = instance.match(/name \= (.*)\;/)
+    const weightClass = instance.match(/weightClass \= (.*)\;/)
+    const weight = instance.match(/interpolationWeight \= (\d*)\;/)
+    return {
+      "style": name && name[1],
+      "weight": weight && weight[1]
+    }
   })
   const obj = {
-    "name": familyName,
+    "name": familyName && familyName[1],
     "interpolations": instances
   }
-  // console.log(obj)
   return obj
 }
