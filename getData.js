7102a09c2a529e3933639fa21e3eec2496baeada
Daniel Gamage <hello@danielgamage.com>
2017-02-27 06:02:11 -0500

    adding query and parsing for designspace files

--- getData.js
+++ getData.js
@@ -1,8 +1,10 @@
+const util = require('util')
 const axios = require('axios')
 const fs = require('fs')
 const ProgressBar = require('progress')
 const blacklist = require('./src/data/blacklist.js')
 const removeDuplicates = require('./src/utils/removeDuplicates.js')
+const parser = require('xml2json')
 
 // YOU MUST SUPPLY YOUR OWN TOKEN FILE
 const config = require('./config.js')
@@ -14,7 +16,8 @@ var options = {
   }
 }
 
-const searchURL = 'https://api.github.com/search/code?q=interpolationWeight+extension:glyphs'
+const glyphsSearchURL      = 'https://api.github.com/search/code?q=interpolationWeight+extension:glyphs'
+const designSpaceSearchURL = 'https://api.github.com/search/code?q=weight+extension:designspace'
 
 const getLink = (header, rel = 'next') => {
   const replacement = `<(.*?)>(?=; rel="${rel}")`
@@ -23,32 +26,55 @@ const getLink = (header, rel = 'next') => {
   return link
 }
 
-async function getGlyphsData() {
+async function getData(url) {
   try {
-    const searchResults = await axios(searchURL, options)
+    const searchResults = await axios(url, options)
     // console.log(getLink(searchResults.headers.link))
 
     const dedupedItems = removeDuplicates(searchResults.data.items, 'repository.full_name')
     console.log(`Deduped ${searchResults.data.items.length - dedupedItems.length} items`)
 
-    const filteredItems = dedupedItems.filter(el => {
-      return (blacklist.indexOf(el.repository.full_name) === -1)
-    })
+    const filteredItems = dedupedItems.filter(el => (
+      blacklist.indexOf(el.repository.full_name) === -1
+    ))
     console.log(`Filtered out ${dedupedItems.length - filteredItems.length} blacklisted items`)
 
     const progress = new ProgressBar('Fetching files [:bar] :percent', {total: dedupedItems.length})
 
     const fontFamilies = Promise.all(filteredItems.map(async (el) => {
       const file = await axios(el.url, options)
-      const fontFamily = parseGlyphsFile(file.data, el)
+      const fontFamily = (url === glyphsSearchURL) ? parseGlyphsFile(file.data, el) : parseDesignSpaceFile(file.data, el)
+
       progress.tick()
-      return fontFamily
+
+      console.log(await fontFamily)
+      return await fontFamily
     }))
-    console.log(await fontFamilies)
-    writeToDisk(await fontFamilies)
+    return await fontFamilies
   } catch (err) {
     console.log('failed', err)
   }
+}
+
+const parseDesignSpaceFile = (fileData, originalQueryMatch) => {
+  const file = new Buffer(fileData.content, 'base64').toString('ascii')
+  const fileObject = parser.toJson(file, {object: true, coerce: true, arrayNotation: ['instance', 'dimension']})
+
+  const familyName = fileObject.designspace.instances.instance[0].familyName
+
+  const instances = fileObject.designspace.instances.instance.map(instance => {
+    const weight = instance.location.dimension.filter(el => el.name.toLowerCase() === "weight")[0].xvalue
+    return {
+      "style": instance.stylename,
+      "weight": weight
+    }
+  })
+  const obj = {
+    "name": familyName && familyName[1],
+    "url": originalQueryMatch.repository.html_url,
+    "interpolations": instances
+  }
+  return obj
 }
 
 const parseGlyphsFile = (fileData, originalQueryMatch) => {
@@ -56,7 +82,7 @@ const parseGlyphsFile = (fileData, originalQueryMatch) => {
   const familyName = file.match(/familyName = "(.*)";/)
   const instanceMatches = file.match(/\{\n?((?:interpolationWeight|customParameters \= \([.\S\s]*?\);)[.\S\s]*?)(?:\}[,\n])/g)
   const instances = instanceMatches.map(instance => {
-    const name = instance.match(/name \= (.*)\;/)
+    const name = instance.match(/name \= (.*)\;\nweightClass/)
     const weightClass = instance.match(/weightClass \= (.*)\;/)
     const weight = instance.match(/interpolationWeight \= (\d*)\;/)
     return {
@@ -84,4 +110,10 @@ const writeToDisk = (data) => {
   });
 }
 
-getGlyphsData()
+async function init() {
+  const glyphsFonts      = await getData(glyphsSearchURL)
+  const designSpaceFonts = await getData(designSpaceSearchURL)
+
+  writeToDisk([...glyphsFonts, ...designSpaceFonts])
+}
+init()
--- src/data/fonts.js
+++ src/data/fonts.js
@@ -30,7 +30,7 @@ module.exports = [
 				"weight": "56"
 			},
 			{
-				"style": "Regular",
+				"style": null,
 				"weight": "79"
 			},
 			{
@@ -56,7 +56,7 @@ module.exports = [
 				"weight": "10"
 			},
 			{
-				"style": "Filter",
+				"style": "\"Hairline Italic\"",
 				"weight": "10"
 			},
 			{
@@ -64,7 +64,7 @@ module.exports = [
 				"weight": "15"
 			},
 			{
-				"style": "Filter",
+				"style": "\"UltraLight Italic\"",
 				"weight": "15"
 			},
 			{
@@ -72,15 +72,15 @@ module.exports = [
 				"weight": "39"
 			},
 			{
-				"style": "Filter",
+				"style": "\"Light Italic\"",
 				"weight": "39"
 			},
 			{
-				"style": "Regular",
+				"style": null,
 				"weight": "56"
 			},
 			{
-				"style": "Filter",
+				"style": null,
 				"weight": "56"
 			},
 			{
@@ -88,11 +88,11 @@ module.exports = [
 				"weight": "77"
 			},
 			{
-				"style": "Filter",
+				"style": "\"Medium Italic\"",
 				"weight": "77"
 			},
 			{
-				"style": "Filter",
+				"style": null,
 				"weight": null
 			},
 			{
@@ -100,15 +100,15 @@ module.exports = [
 				"weight": "121"
 			},
 			{
-				"style": "Filter",
+				"style": "\"Bold Italic\"",
 				"weight": "121"
 			},
 			{
-				"style": "Black",
+				"style": null,
 				"weight": "140"
 			},
 			{
-				"style": "Filter",
+				"style": "\"Black Italic\"",
 				"weight": "140"
 			}
 		]
@@ -118,219 +118,219 @@ module.exports = [
 		"url": "https://github.com/jpt/barlow",
 		"interpolations": [
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "\"Decompose Glyphs\"",
+				"style": null,
 				"weight": null
 			}
 		]
@@ -356,19 +356,19 @@ module.exports = [
 				"weight": "500"
 			},
 			{
-				"style": "Regular",
+				"style": null,
 				"weight": "400"
 			},
 			{
-				"style": "Light",
+				"style": null,
 				"weight": "200"
 			},
 			{
-				"style": "\"Light Extended\"",
+				"style": null,
 				"weight": "200"
 			},
 			{
-				"style": "\"Regular Expanded\"",
+				"style": null,
 				"weight": "400"
 			},
 			{
@@ -388,15 +388,15 @@ module.exports = [
 				"weight": "800"
 			},
 			{
-				"style": "\"Regular SemiExpanded\"",
+				"style": null,
 				"weight": "400"
 			},
 			{
-				"style": "\"Regular SemiCondensed\"",
+				"style": null,
 				"weight": "400"
 			},
 			{
-				"style": "\"Regular UltraExpanded\"",
+				"style": null,
 				"weight": "400"
 			}
 		]
@@ -410,7 +410,7 @@ module.exports = [
 				"weight": "300"
 			},
 			{
-				"style": "Regular",
+				"style": null,
 				"weight": "400"
 			},
 			{
@@ -424,27 +424,27 @@ module.exports = [
 		"url": "https://github.com/iamaaronmartin/federalist",
 		"interpolations": [
 			{
-				"style": "postscriptFontName",
+				"style": "UltraLight",
 				"weight": "40"
 			},
 			{
-				"style": "postscriptFontName",
+				"style": "Light",
 				"weight": "51"
 			},
 			{
-				"style": "postscriptFontName",
+				"style": null,
 				"weight": "66"
 			},
 			{
-				"style": "postscriptFontName",
+				"style": "Medium",
 				"weight": "85"
 			},
 			{
-				"style": "postscriptFontName",
+				"style": "Semibold",
 				"weight": "102"
 			},
 			{
-				"style": "postscriptFontName",
+				"style": "Bold",
 				"weight": "140"
 			}
 		]
@@ -454,40 +454,332 @@ module.exports = [
 		"url": "https://github.com/googlei18n/noto-source",
 		"interpolations": [
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
 			},
 			{
-				"style": "weightClass",
+				"style": null,
 				"weight": null
+			}
+		]
+	},
+	{
+		"url": "https://github.com/sugarlabs/edit-fonts-activity",
+		"interpolations": [
+			{
+				"style": "TestStyleName",
+				"weight": 0
+			}
+		]
+	},
+	{
+		"url": "https://github.com/LettError/MutatorMath",
+		"interpolations": [
+			{
+				"style": "TestStyleName",
+				"weight": 0.6
+			}
+		]
+	},
+	{
+		"url": "https://github.com/metapolator/mutatormathtools",
+		"interpolations": [
+			{
+				"style": "TestStyleName",
+				"weight": 0.6
+			}
+		]
+	},
+	{
+		"url": "https://github.com/SayCV/tools-FDK",
+		"interpolations": [
+			{
+				"style": "TestStyleName",
+				"weight": 0.6
+			}
+		]
+	},
+	{
+		"url": "https://github.com/alif-type/mada",
+		"interpolations": [
+			{
+				"style": "Regular",
+				"weight": 368
+			},
+			{
+				"style": "SemiBold",
+				"weight": 600
+			},
+			{
+				"style": "Bold",
+				"weight": 824
+			}
+		]
+	},
+	{
+		"url": "https://github.com/scribbletone/i-can-variable-font",
+		"interpolations": [
+			{
+				"style": "Medium",
+				"weight": 0.6
+			}
+		]
+	},
+	{
+		"url": "https://github.com/adobe-fonts/source-serif-pro",
+		"interpolations": [
+			{
+				"style": "ExtraLight",
+				"weight": 0
+			},
+			{
+				"style": "Light",
+				"weight": 408
+			},
+			{
+				"style": "Regular",
+				"weight": 1041
+			},
+			{
+				"style": "Semibold",
+				"weight": 1367
+			},
+			{
+				"style": "Bold",
+				"weight": 1722
+			},
+			{
+				"style": "Black",
+				"weight": 2000
+			}
+		]
+	},
+	{
+		"url": "https://github.com/optospinlab/diamondGDS",
+		"interpolations": [
+			{
+				"style": "Medium",
+				"weight": 0.5
+			}
+		]
+	},
+	{
+		"url": "https://github.com/rezerbit/.dotfiles",
+		"interpolations": [
+			{
+				"style": "ExtraLight",
+				"weight": 0
+			},
+			{
+				"style": "Light",
+				"weight": 100
+			},
+			{
+				"style": "Regular",
+				"weight": 368
+			},
+			{
+				"style": "Medium",
+				"weight": 486
+			},
+			{
+				"style": "Semibold",
+				"weight": 600
+			},
+			{
+				"style": "Bold",
+				"weight": 824
+			},
+			{
+				"style": "Black",
+				"weight": 1000
+			}
+		]
+	},
+	{
+		"url": "https://github.com/googlei18n/fontmake",
+		"interpolations": [
+			{
+				"style": "Regular",
+				"weight": 400
+			}
+		]
+	},
+	{
+		"url": "https://github.com/adobe-fonts/adobe-variable-font-prototype",
+		"interpolations": [
+			{
+				"style": "ExtraLight",
+				"weight": 0
+			},
+			{
+				"style": "Light",
+				"weight": 150
+			},
+			{
+				"style": "Regular",
+				"weight": 394
+			},
+			{
+				"style": "Semibold",
+				"weight": 600
+			},
+			{
+				"style": "Bold",
+				"weight": 824
+			},
+			{
+				"style": "Black",
+				"weight": 1000
+			},
+			{
+				"style": "Black Medium Contrast",
+				"weight": 1000
+			},
+			{
+				"style": "Black High Contrast",
+				"weight": 1000
+			}
+		]
+	},
+	{
+		"url": "https://github.com/adobe-fonts/source-hebrew-sans",
+		"interpolations": [
+			{
+				"style": "ExtraLight",
+				"weight": 0
+			},
+			{
+				"style": "Light",
+				"weight": 100
+			},
+			{
+				"style": "Regular",
+				"weight": 368
+			},
+			{
+				"style": "Semibold",
+				"weight": 600
+			},
+			{
+				"style": "Bold",
+				"weight": 824
+			},
+			{
+				"style": "Black",
+				"weight": 1000
+			}
+		]
+	},
+	{
+		"url": "https://github.com/jonpinhorn/atithi",
+		"interpolations": [
+			{
+				"style": "ExtraLight",
+				"weight": 0
+			},
+			{
+				"style": "Light",
+				"weight": 20
+			},
+			{
+				"style": "Regular",
+				"weight": 40
+			},
+			{
+				"style": "Medium",
+				"weight": 60
+			},
+			{
+				"style": "SemiBold",
+				"weight": 80
+			},
+			{
+				"style": "Bold",
+				"weight": 100
+			}
+		]
+	},
+	{
+		"url": "https://github.com/rosettatype/eczar",
+		"interpolations": [
+			{
+				"style": "ExtraBold",
+				"weight": 100
+			}
+		]
+	},
+	{
+		"url": "https://github.com/shannpersand/cooper-type",
+		"interpolations": [
+			{
+				"style": "x184.72",
+				"weight": 165.966835
+			},
+			{
+				"style": "x1429.26",
+				"weight": 429.260254
+			},
+			{
+				"style": "x1697.73",
+				"weight": 697.72678
+			}
+		]
+	},
+	{
+		"url": "https://github.com/adobe-fonts/source-sans-pro",
+		"interpolations": [
+			{
+				"style": "ExtraLight Italic",
+				"weight": 0
+			},
+			{
+				"style": "Light Italic",
+				"weight": 100
+			},
+			{
+				"style": "Italic",
+				"weight": 368
+			},
+			{
+				"style": "Semibold Italic",
+				"weight": 600
+			},
+			{
+				"style": "Bold Italic",
+				"weight": 824
+			},
+			{
+				"style": "Black Italic",
+				"weight": 1000
 			}
 		]
 	}
