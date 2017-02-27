f79ad6612acb625c3c1520bd5e16f57ab0ab1aa7
Daniel Gamage <hello@danielgamage.com>
2017-02-26 22:56:44 -0500

    handle deep property access

--- src/utils/__test__/removeDuplicates.spec.js
+++ src/utils/__test__/removeDuplicates.spec.js
@@ -5,3 +5,8 @@ test('remove items with the same property', () => {
   const expectedArray = [{a: 1, b: 8}, {a: 3, b: 2}]
   expect( removeDuplicates(originalArray, 'b') ).toMatchObject(expectedArray)
 })
+test('handle deep traversal', () => {
+  const originalArray = [{a: { deep: { deeper: 1 } }, b: 8}, {a: { deep: { deeper: 1 } }, b: 3}, {a: { deep: { deeper: 2 } }, b: 6}]
+  const expectedArray = [{a: { deep: { deeper: 1 } }, b: 8}, {a: { deep: { deeper: 2 } }, b: 6}]
+  expect( removeDuplicates(originalArray, 'a.deep.deeper') ).toMatchObject(expectedArray)
+})
--- src/utils/removeDuplicates.js
+++ src/utils/removeDuplicates.js
@@ -1,7 +1,17 @@
-const removeDuplicates = (array, prop) => {
-    return array.filter((el, i, arr) => {
-        return arr.map(mapObj => mapObj[prop]).indexOf(el[prop]) === i;
-    });
+const walk = (el, path) => {
+  const array = path.split('.')
+  array.map(pathSegment => {
+    el = el[pathSegment]
+  })
+  return el
+}
+
+const removeDuplicates = (array, path) => {
+  return array.filter((el, i, arr) => {
+    return arr.map(mapObj => (
+      walk(mapObj, path)
+    )).indexOf(walk(el, path)) === i;
+  });
 }
 
 export default removeDuplicates
