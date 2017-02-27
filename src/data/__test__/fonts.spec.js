803bb5b2ef0ca7a87e8e927349d97a3b98120d9d
Daniel Gamage <hello@danielgamage.com>
2017-02-26 23:49:31 -0500

    more testing

--- src/data/__test__/fonts.spec.js
+++ src/data/__test__/fonts.spec.js
@@ -2,7 +2,12 @@ import fontData from '../fonts.js'
 
 test('no font family names missing', () => {
   fontData.map(font => {
-    expect(font.name).not.toBe(null)
+    expect(font.name).not.toBeNull()
     expect(font.name).not.toBe('')
+  })
+})
+test('no null weights', () => {
+  fontData.map(font => {
+    expect(font.interpolations).not.toBeNull()
   })
 })
